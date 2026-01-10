8a20ac988e7654fc5e1c72af3657ed0d7a5ba99a
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2026-01-10 15:27:15 +0800

    build

--- public/build/assets/message-circle-warning-DxLB8j8v.js
+++
@@ -1,6 +0,0 @@
-import{a as e}from"./app-DUQQ5adV.js";/**
- * @license lucide-react v0.475.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const a=[["path",{d:"M7.9 20A9 9 0 1 0 4 16.1L2 22Z",key:"vv11sd"}],["path",{d:"M12 8v4",key:"1got3b"}],["path",{d:"M12 16h.01",key:"1drbdi"}]],r=e("MessageCircleWarning",a);export{r as M};
